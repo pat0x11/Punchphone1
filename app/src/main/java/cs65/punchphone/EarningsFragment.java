1d426f2f99fa280c107e37fb991e4f404e7a05be
Carter Jacobsen <carter.o.jacobsen.19@dartmouth.edu>
2017-03-01 20:14:43 -0500

    new api added

--- app/src/main/java/cs65/punchphone/EarningsFragment.java
+++ app/src/main/java/cs65/punchphone/EarningsFragment.java
@@ -1,163 +1,57 @@
 package cs65.punchphone;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import lecho.lib.hellocharts.model.Axis;
-import lecho.lib.hellocharts.model.AxisValue;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.LineChartView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
 
 // comment
 // we changed this 
 public class EarningsFragment extends Fragment {
 
-    LineChartView lineChartView;
+    GraphView graphView;
     Context context;
     TableLayout tableLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_earnings, container, false);
-        lineChartView = (LineChartView) view.findViewById(R.id.chart);
-        context = getContext();
+        View view = inflater.inflate(R.layout.fragment_earnings, container, false);
+        graphView = (GraphView) view.findViewById(R.id.graph);
+        context = getActivity().getApplicationContext();
         tableLayout = (TableLayout) view.findViewById(R.id.table_layout);
         drawChart();
         return view;
     }
 
-
     public void drawChart() {
+        DataPoint[] points = new DataPoint[50];
+        for (int i = 0; i < 50; i++) {
+            points[i] = new DataPoint(i, Math.sin(i*0.5) * 20*(Math.random()*10+1));
 
-        List<PointValue> values = new ArrayList<PointValue>(); //the earnings
-        BufferedReader bufferedReader; //reader for the file
-
-        try{
-            String filePath = context.getFilesDir().getPath(); //file path of data
-
-            //Create way to read file
-            FileReader fileReader = new FileReader(filePath);
-            bufferedReader = new BufferedReader(fileReader);
-
-            String line; //line being examined
-            int lineNum = 1; //the index for the data
-
-            //The layout style for the table
-            TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT,TableRow.LayoutParams.WRAP_CONTENT);
-
-            TableRow header = new TableRow(context); //The name of each column
-            header.setLayoutParams(layoutParams);
-
-            //Set up both text views
-            TextView timeTitle = new TextView(context);
-            timeTitle.setGravity(Gravity.CENTER);
-            timeTitle.setPadding(10, 10, 10, 10);
-            TextView stressTitle = new TextView(context);
-            stressTitle.setGravity(Gravity.CENTER);
-            stressTitle.setPadding(10, 10, 10, 10);
-
-            //Title the text views
-            timeTitle.setText("Time");
-            stressTitle.setText("Earnings");
-
-            //Add the text views to the row and the row to the table
-            header.addView(timeTitle);
-            header.addView(stressTitle);
-            tableLayout.addView(header);
-
-            while((line = bufferedReader.readLine()) != null){ //Get a line of data
-
-                String[] split = line.split("\\|"); //Split the line at the defined break
-
-                int temp = Integer.parseInt(split[1]); //get the earnings
-                values.add(new PointValue(lineNum, temp)); //Create new point
-
-                lineNum++; //Increment the index
-
-                //Set up the table data similar to done with the header row but now with data
-                TableRow currentRow = new TableRow(context);
-                currentRow.setLayoutParams(layoutParams);
-
-                TextView time = new TextView(context);
-                time.setGravity(Gravity.CENTER);
-                time.setPadding(10, 10, 10, 10);
-                TextView stress = new TextView(context);
-                stress.setGravity(Gravity.CENTER);
-                stress.setPadding(10, 10, 10, 10);
-
-                time.setText("" + split[0]); //getting from data file
-                stress.setText("" + temp); //data file stress as used above
-                currentRow.addView(time);
-                currentRow.addView(stress);
-
-                tableLayout.addView(currentRow);
-            }
-            bufferedReader.close(); //close the readers to prevent errors
-            fileReader.close();
-        } catch (IOException e){
-            Log.d("Cj", "" + e);
         }
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
 
-        //Set line from all the data points and add to list of lines
-        Line line1 = new Line(values).setColor(Color.BLUE);
-        List<Line> lines = new ArrayList<Line>();
-        lines.add(line1);
-
-        //Put the lines in the chart data
-        LineChartData lineChartData = new LineChartData();
-        lineChartData.setLines(lines);
-
-        //Get the axis values
-        List<AxisValue> xAxisValues = new ArrayList<>();
-        List<AxisValue> axisValuesForY = new ArrayList<>();
-        AxisValue tempValue;
-
-        //X axis is just the number of instances there are
-        for(int i = 0; i <= values.size(); i++){
-            tempValue = new AxisValue(i);
-            tempValue.setLabel(""+i);
-            xAxisValues.add(tempValue);
-        }
-
-        //Y axis is the number of stress levels which is 16
-        for(int i = 0; i < 17; i++){
-            tempValue = new AxisValue(i);
-            tempValue.setLabel(""+i);
-            axisValuesForY.add(tempValue);
-        }
-
-        //Name both axis and set their data
-        Axis xAxis = new Axis(xAxisValues);
-        xAxis.setName("Days");
-        Axis yAxis = new Axis(axisValuesForY);
-        yAxis.setName("Earnings");
-        lineChartData.setAxisXBottom(xAxis);
-        lineChartData.setAxisYLeft(yAxis);
-
-        //apply the data to the chart
-        lineChartView.setLineChartData(lineChartData);
-
+        graphView.addSeries(series);
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMaxX(4);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setScalable(true);
+        graphView.setTitle("Earnings by Date");
+        graphView.setTitleTextSize(100);
+        graphView.setHorizontalScrollBarEnabled(true);
+        GridLabelRenderer labelRenderer = graphView.getGridLabelRenderer();
+        labelRenderer.setHorizontalAxisTitle("Dates");
+        labelRenderer.setVerticalAxisTitle("Earnings in $");
     }
+
 
 }
