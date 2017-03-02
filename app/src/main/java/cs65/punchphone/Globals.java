16eb6490c35dec75e7b711c74aa1b75202ce45ca
jcardwell <jrcardwell16@gmail.com>
2017-03-02 16:03:09 -0500

    Started to link backend to front end

---
+++ app/src/main/java/cs65/punchphone/Globals.java
@@ -0,0 +1,12 @@
+package cs65.punchphone;
+
+/**
+ * Created by Jack on 3/2/17.
+ */
+
+//contains all variables for the app
+public class Globals {
+
+    //url of the backend
+    public static String backendURL="http://10.0.2.2:8080/";
+}
