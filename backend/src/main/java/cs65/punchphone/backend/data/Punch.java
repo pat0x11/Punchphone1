5bc1225e02e7306cb6dcb37423a1140998c06440
Patrick Dellinger <Patrick@patricks-mbp-9.wireless.dartmouth.edu>
2017-03-04 16:04:55 -0500

    added company and punchid to punch datastore

--- backend/src/main/java/cs65/punchphone/backend/data/Punch.java
+++ backend/src/main/java/cs65/punchphone/backend/data/Punch.java
@@ -10,20 +10,27 @@ public class Punch {
 
     public static final String PUNCH_ENTITY_NAME = "Punch";
 
+    public static final String FIELD_PUNCHID = "punchid";
     public static final String FIELD_USERID = "userid";
+    public static final String FIELD_COMPANY = "company";
     public static final String FIELD_PUNCH_IN = "id";
     public static final String FIELD_PUNCH_OUT = "id";
     public static final String FIELD_LOC_LAT = "lat";
     public static final String FIELD_LOC_LONG = "long";
 
+    public String mPunchId;
     public String mUserId;
+    public String mCompany;
     public String mPunchIn;
     public String mPunchOut;
     public String mLatitude;
     public String mLongitude;
 
-    public Punch(String id, String punchIn, String punchOut, String latitude, String longitude){
-        mUserId = id;
+    public Punch(String punchid, String userid, String company,  String punchIn, String punchOut,
+                 String latitude, String longitude) {
+        mPunchId = punchid;
+        mUserId = userid;
+        mCompany = company;
         mPunchIn = punchIn;
         mPunchOut = punchOut;
         mLatitude = latitude;
