65021df178643902185cd3074578c15ba22dc72a
Carter Jacobsen <carter.o.jacobsen.19@dartmouth.edu>
2017-03-05 15:11:38 -0500

    Fixed earnings

--- app/src/main/java/cs65/punchphone/EarningsFragment.java
+++ app/src/main/java/cs65/punchphone/EarningsFragment.java
@@ -3,6 +3,7 @@ package cs65.punchphone;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,13 @@ import android.widget.TableLayout;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
-import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+
+import cs65.punchphone.data.PunchEntry;
+import cs65.punchphone.data.PunchEntryDbHelper;
 
 // comment
 // we changed this 
@@ -20,6 +26,8 @@ public class EarningsFragment extends Fragment {
     GraphView graphView;
     Context context;
     TableLayout tableLayout;
+    private ArrayList<PunchEntry> values;
+    private DataPoint[] points;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,19 +35,27 @@ public class EarningsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_earnings, container, false);
         graphView = (GraphView) view.findViewById(R.id.graph);
         context = getActivity().getApplicationContext();
-        tableLayout = (TableLayout) view.findViewById(R.id.table_layout);
+        Log.d("Earnings", "loading data next");
+        PunchEntryDbHelper punchEntryDbHelper = new
+                PunchEntryDbHelper(getActivity());
+        values = punchEntryDbHelper.fetchEntries();
+        Log.d("Earnings", "Number of values " + values.size());
+        points = new DataPoint[values.size()];
+
+        for (int i = 0; i < values.size(); i++) {
+            points[i] = new DataPoint(i + 1, values.get(i).getEarnings
+                    ());
+
+        }
+
         drawChart();
         return view;
     }
 
     public void drawChart() {
-        DataPoint[] points = new DataPoint[50];
-        for (int i = 0; i < 50; i++) {
-            points[i] = new DataPoint(i, Math.sin(i*0.5) * 20*(Math.random()*10+1));
+        Log.d("Graphing", points[0].toString());
 
-        }
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
-
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
         graphView.addSeries(series);
         graphView.getViewport().setXAxisBoundsManual(true);
         graphView.getViewport().setMaxX(4);
